90e49f7f73c9ecae5d902ff74a010d6694a1883a
calum <calum@semmle.com>
2019-04-26 11:37:47 +0100

    Add project files.

---
+++ Properties/Properties.cpp
@@ -0,0 +1,12 @@
+﻿// Properties.cpp : Defines the entry point for the application.
+//
+
+#include "Properties.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
